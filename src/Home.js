b396a461a108616011c6913290e4797e99bedaa3
AJ Gambino <66279789+ajGambino@users.noreply.github.com>
2023-05-23 13:34:15 -0400

    testing DB rendering

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,43 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { firestore } from './firebase';
 
 const Home = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        // Fetch users' data from Firestore
+        const fetchUsers = async () => {
+            try {
+                const usersSnapshot = await firestore.collection('users').get();
+                const usersData = usersSnapshot.docs.map((doc) => doc.data());
+                setUsers(usersData);
+            } catch (error) {
+                console.log('Error fetching users:', error);
+            }
+        };
+
+        fetchUsers();
+    }, []);
+
     return (
         <div>
             <h2>Home</h2>
-            <p>Welcome to the Golf Betting App!</p>
-            <p>Click the button below to view the results.</p>
-            <Link to="/results">View Results</Link>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Total Net Amount</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map((user) => (
+                        <tr key={user.id}>
+                            <td>{user.name}</td>
+                            <td>{user.totalNetAmount}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 };
